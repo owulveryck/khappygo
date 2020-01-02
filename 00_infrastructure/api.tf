c67f231c99efb3fa2916560e5c0da483f90f4fdd
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2020-01-02 14:35:08 +0100

    feat: version ok

---
+++ 00_infrastructure/api.tf
@@ -0,0 +1,12 @@
+resource "google_project_service" "k8s_api" {
+  service = "container.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "cloudresourcemanager_api" {
+  service = "cloudresourcemanager.googleapis.com"
+
+  disable_dependent_services = true
+}
+
